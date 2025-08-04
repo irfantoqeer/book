2eb1c1d9c475f280e4a56419f2c3ecfe579983ba
Irfan Toqeer <arfan884@gmail.com>
2025-08-05 00:24:32 +0500

    Add files via upload

---
+++ Script (18).js
@@ -0,0 +1,11 @@
+// scripts.js
+
+// Example: Adding a console message for testing
+console.log("My Book JavaScript Loaded");
+
+// Example function that could be used for interactive behavior (e.g., changing content dynamically)
+function showChapterAlert() {
+    alert("Welcome to Chapter 1!");
+}
+
+// You can call this function on certain events if desired, such as button clicks or page load
